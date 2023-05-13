b47d0b462da302b454eac677f4979ce80aefd852
jarcv <jarcv@iscte-iul.pt>
2023-05-13 19:14:36 +0100

    a

--- sitemestrado/static/js/script.js
+++ sitemestrado/static/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+    $("#btn2").click(function () {
+        $('#future').show();
+        $('#past').hide();
+    });
+});
+$(document).ready(function () {
     $("#btn1").click(function () {
+        $('#future').hide();
         $('#past').show();
     });
 });
